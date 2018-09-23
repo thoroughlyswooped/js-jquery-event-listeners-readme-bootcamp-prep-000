7ff87462a189dc876a9395567de0f86500f089f0
Ezra T Huffman <joshooha@gmail.com>
2018-09-23 18:51:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,14 @@
-//define functions here
+function getIt() {
+  $("p").on("click", function(e) {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    this.prepend('<img class = "tasty">');
+  })
+}
 
 $(document).ready(function(){
 
