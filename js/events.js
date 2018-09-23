5d6f8ef57d6019df6a535cf67a0ea76a2848877b
Ezra T Huffman <joshooha@gmail.com>
2018-09-23 19:17:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
+function getIt() {
+  $("p").on("click", function(e) {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e){
+    if (e.which === 71){
+      alert('you have pressed the "G" key')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+    return;
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
